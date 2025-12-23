43d01661510e8892dd650b742da1ce70f565c689
nishanthr21 <153345642+nishanthr21@users.noreply.github.com>
2025-12-23 09:12:36 +0530

    Add files via upload

---
+++ SB002/SB002/src/main/java/com/example/SB002/helloController.java
@@ -0,0 +1,20 @@
+package com.example.SB002;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+@Autowired
+    helloService h;
+
+    @GetMapping("/")
+    public String greet(){
+        return h.wish();
+    }
+    @GetMapping("/about")
+    public String caller(){
+        return h.admit();
+    }
+}
---
+++ SB002/SB002/src/main/java/com/example/SB002/helloService.java
@@ -0,0 +1,14 @@
+package com.example.SB002;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class helloService {
+    public String wish(){
+        return "hello nanba";
+    }
+
+    public String admit(){
+        return "welcome to cbe";
+    }
+}
