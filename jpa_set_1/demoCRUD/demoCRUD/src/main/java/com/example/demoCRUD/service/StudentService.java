1bf350e272b209b898661c49db5b1566bebeb0dd
nishanthr21 <153345642+nishanthr21@users.noreply.github.com>
2025-12-23 09:16:40 +0530

    Add files via upload

---
+++ jpa_set_1/demoCRUD/demoCRUD/src/main/java/com/example/demoCRUD/service/StudentService.java
@@ -0,0 +1,44 @@
+package com.example.demoCRUD.service;
+
+import com.example.demoCRUD.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    List<Student> s = new ArrayList<>(
+    Arrays.asList(
+            new Student(1,"Aathil","Hecker"),
+            new Student(2,"Suganth","Web Destroyer"))
+
+    );
+
+
+    public List<Student> getStudent() {
+
+        return s;
+
+    }
+
+    public Student getStudentByRollno(int rollno){
+        int index=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s.get(i).getNo()==rollno)
+            {
+                index=i;
+                break;
+            }
+        }
+        return s.get(index);
+    }
+
+    public String setStudent(Student stu) {
+        s.add(stu);
+        return "sucess";
+    }
+}
